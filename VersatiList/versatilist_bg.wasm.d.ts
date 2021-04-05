7f2b0341428cd341cb21be3c4a066db91915ab2d
Olivier ROLAND <newca12@users.noreply.github.com>
2021-04-05 14:34:07 +0200

    Add VersatiList WASM WebApp

---
+++ VersatiList/versatilist_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main(a: number, b: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1a090b75bb801f6f(a: number, b: number): void;
+export function _dyn_core__ops__function__Fn__A_B_C___Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1e6955804d389887(a: number, b: number, c: number, d: number, e: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hd01847630c5e50f5(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__hf763b39831b1168f(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_start(): void;
