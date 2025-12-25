6357f42af94eb7dc7dbf0dcce88b0e29e4ca22e6
newca12 <newca12@users.noreply.github.com>
2025-12-25 23:11:08 +0000

    Update recipes

--- GourmandWebViewer/gourmand-web-viewer_bg.wasm.d.ts
+++ GourmandWebViewer/gourmand-web-viewer_bg.wasm.d.ts
@@ -1,13 +1,24 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export function main(a: number, b: number): number;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number): number;
-export const __wbindgen_export_2: WebAssembly.Table;
-export function wasm_bindgen__convert__closures__invoke3__haac977f21675dede(a: number, b: number, c: number, d: number, e: number): void;
-export function wasm_bindgen__convert__closures__invoke0_mut__h72400522de868b4b(a: number, b: number): void;
-export function wasm_bindgen__convert__closures__invoke1_mut__hdebdc84142f3c6be(a: number, b: number, c: number): void;
-export function __wbindgen_exn_store(a: number): void;
-export function wasm_bindgen__convert__closures__invoke2_mut__h104e882ef65550b9(a: number, b: number, c: number, d: number): void;
-export function __wbindgen_start(): void;
+export const main: (a: number, b: number) => number;
+export const rust_zstd_wasm_shim_calloc: (a: number, b: number) => number;
+export const rust_zstd_wasm_shim_free: (a: number) => void;
+export const rust_zstd_wasm_shim_malloc: (a: number) => number;
+export const rust_zstd_wasm_shim_memcmp: (a: number, b: number, c: number) => number;
+export const rust_zstd_wasm_shim_memcpy: (a: number, b: number, c: number) => number;
+export const rust_zstd_wasm_shim_memmove: (a: number, b: number, c: number) => number;
+export const rust_zstd_wasm_shim_memset: (a: number, b: number, c: number) => number;
+export const rust_zstd_wasm_shim_qsort: (a: number, b: number, c: number, d: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h3f75395dadc00be0: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__closure__destroy__h0d49f4ce7e7b4e75: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__hff165a7794cfda0f: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h2b940c6ad47ff8ed: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__closure__destroy__h51858c4b7b8e0376: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h3400c591dad39498: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_start: () => void;
